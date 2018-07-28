563e3afa40ab8fe7ac701c233d588ae71543ccc2
hamedRahimvand <hamed.rahimvand@gmail.com>
2018-07-28 21:44:50 +0430

    Initial commit

---
+++ app/src/main/java/com/hamed/navigation/UserFragment.kt
@@ -0,0 +1,34 @@
+package com.hamed.navigation
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_user.view.*
+
+class UserFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bundle = Bundle()
+        bundle.putString("test","hamed")
+        view.button.setOnClickListener {
+            //view.findNavController().navigate(R.id.toAccount)
+            Navigation.findNavController(view).navigate(R.id.toAccount,bundle)
+        }
+
+
+    }
+
+
+}
